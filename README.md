2beab581672aead6e14323593a698af9ca59a888
Ben Harper <rogojin@gmail.com>
2018-11-21 13:56:40 +0200

    Add new mode for rendering to PNG

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # imqs-html2pdf
-Small node express/puppeteer server that receives html via a post request and renders the html in chromuim-headerless and returns the resulting pdf.
+Small node express/puppeteer server that receives html via a post request, uses Chrome Headless to render the document to PDF or PNG.
 
 # Run Server
 Install dependencies: `npm install`
 
 Start server: `npm start`
+
+# Examples
+
+    curl -H "Content-Type:text/html" -d "<h1>Hello world</h1>" "localhost:8080/render?format=pdf" --output test.pdf
+    curl -H "Content-Type:text/html" -d "<h1>Hello world</h1>" "localhost:8080/render?format=png" --output test.png
+    curl -H "Content-Type:text/html" -d @example.html "localhost:8080/render?format=pdf" --output test.pdf
 
 # Docker
 
--- server.js
+++ server.js
@@ -13,36 +13,80 @@ app.use(function (req, res, next) {
 	if (req.is('text/*')) {
 		req.text = '';
 		req.setEncoding('utf8');
-		req.on('data', function (chunk) { req.text += chunk });
+		req.on('data', function (chunk) {
+			req.text += chunk
+		});
 		req.on('end', next);
 	} else {
 		next();
 	}
 });
 
-app.post('/html2pdf', (req, res) => {
+function render(req, res) {
+	if (req.text === undefined) {
+		res.status(400).send('The body must have Content-Type:text/html');
+		return;
+	}
+
 	var html = req.text;
+	var format = req.query['format'] || 'pdf';
 	var pageSize = req.query['pagesize'] || 'A4';
 	var pageLandscape = req.query['pagelandscape'] === 'true';
+	var width = req.query['width'] || 210 * 4;
+	var height = req.query['height'] || 297 * 4;
+	var deviceScaleFactor = req.query['deviceScaleFactor'] || 1.0;
+
+	width = parseInt(width);
+	height = parseInt(height);
+	deviceScaleFactor = parseFloat(deviceScaleFactor);
 
 	(async () => {
 		try {
-			const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+			const browser = await puppeteer.launch(
+				{ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
 			const page = await browser.newPage();
 			await page.setContent(html);
 
-			await page.pdf({ format: pageSize, landscape: pageLandscape, printBackground: true })
-				.then((value) => {
-					res.setHeader("content-type", "application/pdf");
+			if (format == 'pdf') {
+				await page.pdf({
+					format: pageSize,
+					landscape: pageLandscape,
+					printBackground: true
+				}).then((value) => {
+					res.setHeader('content-type', 'application/pdf');
 					res.send(value);
 				});
+			} else if (format == 'png') {
+				await page.setViewport({
+					width: width,
+					height: height,
+					deviceScaleFactor: deviceScaleFactor,
+				});
+
+				await page.screenshot({
+					type: 'png',
+					omitBackground: true,
+					fullPage: true
+				}).then(data => {
+					res.setHeader('content-type', 'image/png');
+					res.send(data);
+				});
+			} else {
+				res.status(400).send(`Unknown format ${format}. Valid formats are pdf,png`);
+			}
 
 			await browser.close();
 		} catch (error) {
 			console.error(error);
+			res.status(400).send(error);
 		}
 	})();
-});
+}
+
+// The original API was called html2pdf, but this has grown to be able to produce PNGs,
+// so that's why it got another name "render".
+app.post('/html2pdf', render);
+app.post('/render', render);
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
